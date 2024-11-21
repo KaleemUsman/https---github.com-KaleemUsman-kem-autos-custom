3c1955605c4231bc6863a78e6d7d2957105b4e3c
KaleemUsman <160784378+KaleemUsman@users.noreply.github.com>
2024-11-21 23:54:48 +0500

    Update page.tsx

--- src/app/latest/page.tsx
+++ src/app/latest/page.tsx
@@ -6,11 +6,11 @@ const latest = () => {
        <section className='services' style={{backgroundImage: "url(latest.jpg)"}}>
        <div className='latest-content'>
         <h2 className='fade-in'>Discover the Latest Vehicles at KeM Autos</h2>
-        <p>Step into the future of driving with our latest arrivals at KeM Autos. From cutting-edge technology to sleek designs, our newest vehicles are crafted to deliver unparalleled performance, comfort, and style.</p>
+        <p className='fade-in'>Step into the future of driving with our latest arrivals at KeM Autos. From cutting-edge technology to sleek designs, our newest vehicles are crafted to deliver unparalleled performance, comfort, and style.</p>
 
-<p>Whether you’re seeking the thrill of a sports car, the reliability of a family SUV, or the luxury of a premium sedan, our showroom features a curated selection to meet every need. Each model is equipped with the latest innovations to ensure a driving experience that’s as safe as it is exciting.</p>
+<p className='fade-in'>Whether you’re seeking the thrill of a sports car, the reliability of a family SUV, or the luxury of a premium sedan, our showroom features a curated selection to meet every need. Each model is equipped with the latest innovations to ensure a driving experience that’s as safe as it is exciting.</p>
 
-<p>Explore our latest lineup and redefine what it means to drive in style—your next adventure starts here!</p>
+<p className='fade-in'>Explore our latest lineup and redefine what it means to drive in style—your next adventure starts here!</p>
       </div>
       </section>
     </div>
