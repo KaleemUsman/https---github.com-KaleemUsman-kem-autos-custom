5f3529a5a2907166c8c9087f80433ffda0aee2f4
KaleemUsman <160784378+KaleemUsman@users.noreply.github.com>
2024-11-21 23:55:11 +0500

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -6,17 +6,17 @@ const services = () => {
        <section className='services' style={{backgroundImage: "url(services.jpg)"}}>
        <div className='services-content'>
         <h2 className='fade-in'>Our Services at KeM Autos</h2>
-<p>Vehicle Sales: Explore a wide range of high-quality cars, from luxury to everyday models, designed to suit your lifestyle and budget.</p>
+<p className='fade-in'>Vehicle Sales: Explore a wide range of high-quality cars, from luxury to everyday models, designed to suit your lifestyle and budget.</p>
 
-<p>Personalized Consultations: Our expert team helps you make informed decisions, ensuring you drive away with the perfect vehicle.</p>
+<p className='fade-in'>Personalized Consultations: Our expert team helps you make informed decisions, ensuring you drive away with the perfect vehicle.</p>
 
-<p>Financing Solutions: Flexible financing options tailored to make owning your dream car easier than ever.</p>
+<p className='fade-in'>Financing Solutions: Flexible financing options tailored to make owning your dream car easier than ever.</p>
 
-<p>Maintenance & Repairs: Keep your vehicle in top condition with our trusted maintenance and repair services.</p>
+<p className='fade-in'>Maintenance & Repairs: Keep your vehicle in top condition with our trusted maintenance and repair services.</p>
 
-<p>Trade-Ins: Upgrade your ride by trading in your current vehicle for a great value.</p>
+<p className='fade-in'>Trade-Ins: Upgrade your ride by trading in your current vehicle for a great value.</p>
 
-<p>At KeM Autos, we prioritize your satisfaction, ensuring every service is delivered with professionalism and care. Visit us and experience automotive excellence!</p>
+<p className='fade-in'>At KeM Autos, we prioritize your satisfaction, ensuring every service is delivered with professionalism and care. Visit us and experience automotive excellence!</p>
       </div>
       </section>
       </div>
