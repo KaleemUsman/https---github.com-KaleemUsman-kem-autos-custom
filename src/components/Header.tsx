cb8340f417a0c8a06fd3934113fc4438a2038aff
KaleemUsman <160784378+KaleemUsman@users.noreply.github.com>
2024-11-22 03:57:54 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,16 @@ import { FaCar } from 'react-icons/fa'
 const Header = () => {
   return (
     <header className='header'>
-<h1 className='logo'>KeM Autos</h1>
-<nav>
-<Link href="/">Home</Link>
-<Link href="/about">About</Link>
-<Link href="/services">Services</Link>
-<Link href="/latest">Latest</Link>
-<Link href="/contact">Contact</Link>
-    <FaCar style={{color: 'yellow', fontSize: '18px'}} />
-</nav>
+      
+<h1 className='logo'>
+<FaCar style={{color: 'yellow', fontSize: '18px',}} /> KeM Autos</h1>
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/services">Services</Link>
+      <Link href="/latest">Latest</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
 
     </header>
   )
