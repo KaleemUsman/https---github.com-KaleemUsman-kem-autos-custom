4becc6104bdc5b67e0c756de2952f05fa2b4c131
KaleemUsman <160784378+KaleemUsman@users.noreply.github.com>
2024-11-21 23:53:10 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,11 +6,11 @@ const about = () => {
       <section className='about' style={{backgroundImage: "url(about.jpg)"}}>
         <div className='about-content'>
           <h2 className='fade-in'>About KeM Autos</h2>
-        <p>Welcome to KeM Autos, where passion meets excellence in the world of vehicles. We take pride in offering a diverse collection of high-quality cars, tailored to meet the needs of every driver. Whether you’re searching for style, performance, or functionality, our showroom is designed to help you find your perfect match.</p>
+        <p className='fade-in'>Welcome to KeM Autos, where passion meets excellence in the world of vehicles. We take pride in offering a diverse collection of high-quality cars, tailored to meet the needs of every driver. Whether you’re searching for style, performance, or functionality, our showroom is designed to help you find your perfect match.</p>
 
-<p>With a commitment to exceptional service, KeM Autos is more than just a place to shop—it’s an experience. Our knowledgeable team is dedicated to guiding you every step of the way, ensuring a seamless journey from selection to ownership.</p>
+<p className='fade-in'>With a commitment to exceptional service, KeM Autos is more than just a place to shop—it’s an experience. Our knowledgeable team is dedicated to guiding you every step of the way, ensuring a seamless journey from selection to ownership.</p>
 
-<p>Discover the difference at KeM Autos, where your dream vehicle awaits!</p>
+<p className='fade-in'>Discover the difference at KeM Autos, where your dream vehicle awaits!</p>
 </div>
       </section>
     </div>
